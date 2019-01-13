16153cdcadde625d17b864c5b298f6ee95e77f44
Andrew Napierkowski <andrew@fantasydata.com>
2019-01-13 16:33:14 -0500

    more doodly

--- DoodleShader/Scenes/TestScene.cs
+++ DoodleShader/Scenes/TestScene.cs
@@ -26,10 +26,18 @@ namespace DoodleShader.Scenes
 
             doodleEffect = content.Load<Effect>("effects/DoodleShader");
             doodleEffect.Parameters["doodleMaxOffset"].SetValue(new Vector2(0.005f, 0.005f));
-            doodleEffect.Parameters["doodleNoiseScale"].SetValue(new Vector2(2f, 2f));
-            doodleEffect.Parameters["doodleFrameCount"].SetValue(10);
+            doodleEffect.Parameters["doodleNoiseScale"].SetValue(new Vector2(6f, 8f));
+            doodleEffect.Parameters["doodleFrameCount"].SetValue(6);
             doodleEffect.Parameters["doodleFrameTime"].SetValue(0.2f);
             emmaSprite.material = new Material(doodleEffect);
+
+
+            var text = createEntity("txt");
+            var textTexture = content.Load<Texture2D>("img/text");
+            var textSprite = new Sprite(textTexture);
+            text.addComponent(textSprite);
+            text.position = new Vector2(400, 600);
+            textSprite.material = new Material(doodleEffect);
 
 
         }
